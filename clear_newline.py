e9ee7f78c32782a4812f9ab81c9d1892ec059386
LucasZhang58 <lucas.zhang58@gmail.com>
2023-02-05 09:48:58 -0500

    deleting a few files

--- clear_newline.py
+++
@@ -1,8 +0,0 @@
-def remove_newlines(file_path):
-    with open(file_path, "r") as file:
-        content = file.read().replace("\n", "")
-    with open(file_path, "w") as file:
-        file.write(content)
-
-# Example usage
-remove_newlines("meditations_raw.txt")
