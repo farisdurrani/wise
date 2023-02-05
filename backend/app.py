03006870aae2ca73ad04c9d22725e18752538c95
Faris Durrani <afarisdurrani@gmail.com>
2023-02-04 21:53:23 -0500

    Added bag of words comparison

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import os
 import json
+from process import find_best_sentence
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +14,7 @@ if PORT is not None:
 else:
     PORT = 8000
 
+
 @app.route("/")
 def hello_world():
     return {"hello": "world"}
@@ -20,10 +22,10 @@ def hello_world():
 
 @app.route("/api/question", methods=["POST", "GET"])
 def process_question():
-    question = request.args.getlist('question')
-    return {"question": 12}
+    question = request.args.getlist("question")
+    best_sentence = find_best_sentence(question)
+    return {"best_sentence": best_sentence}
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
-
--- backend/process.py
+++ backend/process.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import json
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 
-with open("../data/stopwords.json") as f:
+INPUT_COL_NAME = "Text"
+
+with open("stopwords.json") as f:
     stopWordsList = json.load(f)["stopwords"]
 
 stopWordsSet = set(stopWordsList)
+df = pd.read_csv("meditations.csv")
+input_bag_of_words = create_input_bag_of_words(df)
 
-def create_bag_of_words(text):
-    words = [word.lower() for word in text.replace("[.?!\"':;%*@#]", "").split(" ") if word not in stopWordsSet]
+def create_bag_of_words(text: str) -> dict:
+    """
+    Creates a bag of words from a text
+    :param text: The text to create the bag of words from
+    :return: A dictionary with the words as keys and the number of occurrences as values
+    """
+    words = [word.lower() for word in text.translate({ord(c): None for c in ".?!\"':;%*@[]("}).split(" ") if word not in stopWordsSet]
     bag_of_words = {}
     for word in words:
         if word in bag_of_words:
@@ -17,10 +26,43 @@ def create_bag_of_words(text):
             bag_of_words[word] = 1
     return bag_of_words
 
-if __name__ == "__main__":
-  df = pd.read_csv("../data/meditations.csv")
+def compare_bag_of_words(bag_of_words_1: dict, bag_of_words_2: dict) -> int:
+    """
+    Compares two bag of words
 
-  with Pool(5) as p:
-      ee = p.map(create_bag_of_words, df["Text"])
-      print(ee[0])
+    :param bag_of_words_1: The first bag of words
+    :param bag_of_words_2: The second bag of words
+    :return: The similarity of the two bag of words
+    """
+    similarity = 1
+    for word1 in bag_of_words_1.keys():
+        if word1 in bag_of_words_2:
+            similarity += bag_of_words_1[word1] * bag_of_words_2[word1]
+    return similarity
+
+def find_best_sentence(question: str) -> str:
+    """
+    Finds the best sentence from the input bag of words
+    :param question: The question to find the best sentence for
+    :return: The best sentence
+    """
+    question_bag_of_words = create_bag_of_words(question)
+    best_sentence_i = -1
+    best_sentence_similarity = 0
+    for i, input_bow in enumerate(input_bag_of_words):
+        similarity = compare_bag_of_words(input_bow, question_bag_of_words)
+        if similarity > best_sentence_similarity:
+            best_sentence_similarity = similarity
+            best_sentence = i
+    return df[INPUT_COL_NAME][i]
+
+def create_input_bag_of_words(df: pd.DataFrame) -> list:
+    """
+    Creates a list of bag of words from a dataframe
+    :param df: The dataframe to create the bag of words from
+    :return: A list of bag of words
+    """
+    with Pool(cpu_count()) as p:
+        input_bag_of_words = p.map(create_bag_of_words, df[INPUT_COL_NAME])
+    return input_bag_of_words
 
